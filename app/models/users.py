ec018bcde5f64b2468de9b743a4810fea70d52ac
lvcong <cong.lv.yx@gmail.com>
2017-01-22 22:22:59 +0800

    update models db and delete something

---
+++ app/models/users.py
@@ -0,0 +1,16 @@
+from passlib.apps import custom_app_context as pwd_context
+
+
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(32), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
