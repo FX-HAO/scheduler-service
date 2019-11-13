346af5b261ae345033dbccc516c979f9c75fff71
CoCong <cong.lv.yx@gmail.com>
2019-11-13 23:59:11 +0800

    add hash password

--- scheduler_service/models/user.py
+++ scheduler_service/models/user.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import time
 
+from passlib.hash import pbkdf2_sha256
 import orm
 
 from . import metadata
@@ -12,7 +13,7 @@ class User(orm.Model):
 
     id = orm.Integer(primary_key=True)
     name = orm.String(max_length='32')
-    password_hash = orm.String(max_length='128')
+    password_hash = orm.String(max_length='256')
     email = orm.String(max_length='32')
     verify = orm.Boolean(default=False)
     register_time = orm.DateTime(default=datetime.now)
@@ -20,3 +21,14 @@ class User(orm.Model):
 
     async def ping(self):
         await self.update(login_time=time.time())
+
+    @property
+    def password(self, password):
+        raise AttributeError('password is not a readable attr')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = pbkdf2_sha256.hash(password)
+
+    def verify_password(self, password):
+        return pbkdf2_sha256.verify(password, self.password_hash)
